dc7f3347d507454bbbec3bd36f3c17ab2a5e8d0f
McAlec <hello@mcalec.dev>
2024-09-11 21:14:26 -0400

    fix/push

--- src/main/java/net/mcalec/mcalecs/item/SuperMarioGalaxyGustyGardenGalaxyItem.java
+++ src/main/java/net/mcalec/mcalecs/item/SuperMarioGalaxyGustyGardenGalaxyItem.java
@@ -1,22 +1,21 @@
 
 package net.mcalec.mcalecs.item;
 
-import net.minecraftforge.registries.ForgeRegistries;
-
-import net.minecraft.world.level.Level;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.RecordItem;
-import net.minecraft.world.item.Rarity;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Item;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.network.chat.Component;
-
 import java.util.List;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.RecordItem;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class SuperMarioGalaxyGustyGardenGalaxyItem extends RecordItem {
 	public SuperMarioGalaxyGustyGardenGalaxyItem() {
-		super(0, () -> ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("mcalecs:super_mario_galaxy_gusty_garden_galaxy")), new Item.Properties().stacksTo(1).rarity(Rarity.RARE), 100);
+		super(0, () -> ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("mcalecs:super_mario_galaxy_gusty_garden_galaxy")), new Item.Properties().stacksTo(1).rarity(Rarity.RARE), 9360);
 	}
 
 	@Override
